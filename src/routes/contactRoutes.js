da4f81cb94a7476c56a10d18d860bb3a059d29df
harshaldwarade-del <harshaldwarade@gmail.com>
2026-05-14 15:04:13 +0530

    Update contactRoutes.js

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -9,22 +9,25 @@ const {
   markAsProspect,
   unmarkProspect,
 } = require("../controllers/contactController");
-const { protect, authorise } = require("../middleware/auth");
+const { protect, restrictTo } = require("../middleware/auth");
 
 // All contact routes require authentication
 router.use(protect);
 
-router.route("/")
-  .get(getContacts)
-  .post(createContact);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id")
+router
+  .route("/:id")
   .get(getContact)
   .put(updateContact)
   .delete(authorise("admin"), deleteContact);
 
 // Prospect actions on a contact
 router.patch("/:id/mark-prospect", markAsProspect);
-router.patch("/:id/unmark-prospect", authorise("admin", "user"), unmarkProspect);
+router.patch(
+  "/:id/unmark-prospect",
+  restrictTo("admin", "user"),
+  unmarkProspect,
+);
 
 module.exports = router;
